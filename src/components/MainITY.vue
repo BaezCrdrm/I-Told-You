2b66da778aa763d9b0c0a5c66ba761359500bf65
BaezCrdrm <dev.baez_crdrm@outlook.com>
2019-08-24 00:26:09 -0500

    Main idea

---
+++ src/components/MainITY.vue
@@ -0,0 +1,64 @@
+<template>
+    <!-- https://developer.microsoft.com/en-us/fabric#/styles/web/layout -->
+    <div class="ms-Grid" dir="ltr">
+        <div class="ms-Grid-row">
+            <div class="emoji item ms-Grid-col ms-hiddenSm ms-md3 ms-lg2">{{ message.emoji1 }}</div>
+
+            <div class="main-content item ms-Grid-col ms-sm12 ms-md6 ms-lg8">
+                <div class="i-told-you">
+                    <h1 class="main-message">{{ message.main }}</h1>
+                    <h2 class="comp-message">{{ message.comp }}</h2>
+
+                    <div class="opt-message">
+                        <p>{{ message.msg }}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="emoji item ms-Grid-col ms-hiddenMdUp ms-sm6">{{ message.emoji1 }}</div>
+            <div class="emoji item ms-Grid-col ms-sm6 ms-md3 ms-lg2">{{ message.emoji2 }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            message: {
+                main: "Te dije que haría la aplicación",
+                comp: "Le veo bastante utilidad 😬",
+                msg: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias sit vitae aliquam mollitia eius, animi ipsam, perspiciatis repellendus repudiandae totam earum maxime repellat consectetur, eligendi iste itaque! Ullam, mollitia architecto.",
+
+                emoji1: "😁",
+                emoji2: "😎"
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.item {
+    /* background-color: gray; */
+}
+
+.main-message {
+
+}
+
+.comp-message {
+
+}
+
+.emoji {
+    padding-top:35px;
+    font-size: 60px;
+}
+
+.opt-message > p {
+    text-align: justify;
+    margin-left: 10px;
+    margin-right: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainITY />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MainITY from '@/components/MainITY.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    MainITY
   }
 }
 </script>

29444e77c1337c4e5731b470dad039cf7c45c7c3
BaezCrdrm <dev.baez_crdrm@outlook.com>
2019-09-13 10:23:58 -0500

    Alpha 1

---
+++ precache-manifest.8dd14ba80585bef230be4f811d26a553.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c591e6a61e17ada9f67f",
+    "url": "/js/about.54f625cf.js"
+  },
+  {
+    "revision": "1ac2d0f2b661204efc67",
+    "url": "/css/app.70950a1b.css"
+  },
+  {
+    "revision": "1ac2d0f2b661204efc67",
+    "url": "/js/app.d5d464b1.js"
+  },
+  {
+    "revision": "5abd1395ffdcafd409dd",
+    "url": "/js/chunk-vendors.3475109e.js"
+  },
+  {
+    "revision": "8805415ade8137713dce017380add4cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];

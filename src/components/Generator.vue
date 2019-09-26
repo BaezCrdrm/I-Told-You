5f083502cd4ba1ec0bd8df20f631a7942938d324
BaezCrdrm <dev.baez_crdrm@outlook.com>
2019-09-26 13:18:20 -0500

    Copy to clipboard

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -1,32 +1,31 @@
 <template>
-    <div class="ms-Grid" dir="ltr">
-        <div class="ms-Grid-row">
-            <div class="setup-content ms-Grid-col ms-sm12 ms-md6">
-                <h1>Generator</h1>
-                <div>
-                    <!-- <TextField label="Title" placeholder="I told you..." />
-                    <TextField label="Complementary" placeholder="Complementary information" />
-                    <TextField label="Optional message" multiline rows="3" placeholder="Optional message to give a little more of context..." />
-                    <TextField label="Standard" multiline rows={3} /> -->
-
-                    <input label="Title" v-model="message.main" placeholder="I told you..." /><br>
-                    <input label="Complementary" v-model="message.comp" placeholder="Complementary information" /><br>
-                    <input label="Optional message" v-model="message.msg" multiline rows="3" placeholder="Optional message to give a little more of context..." /><br>
-                    <!-- <input label="Standard" multiline rows={3} /> -->
-                    <button @click="updateQuery">Generate</button>
-                    <p>{{ preview }}</p>
+    <div>
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+        <div class="ms-Grid" dir="ltr">
+            <div class="ms-Grid-row">
+                <div class="setup-content ms-Grid-col ms-sm12 ms-md6">
+                    <h1>Generator</h1>
+                    <div>
+                        <input label="Title" v-model="message.main" placeholder="I told you..." /><br>
+                        <input label="Complementary" v-model="message.comp" placeholder="Complementary information" /><br>
+                        <input label="Optional message" v-model="message.msg" multiline rows="3" placeholder="Optional message to give a little more of context..." /><br>
+                        <input v-if="exists" type="text" id="preview_link" v-model="preview" /><br>
+                        <button @click="updateQuery">Generate</button>
+                    </div>
                 </div>
-            </div>
 
-            <div class="preview-content ms-Grid-col ms-sm12 ms-md6">
-                <MainITY :pmsg="objToString()" />
+                <div class="preview-content ms-Grid-col ms-sm12 ms-md6">
+                    <MainITY :pmsg="objToString()" />
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import Fabric from '@johannes-z/office-ui-fabric-vue';
 import MainITY from '@/components/MainITY.vue'
 
 export default {
@@ -42,11 +41,11 @@ export default {
                 emoji2: ''
             },
             query: '',
-            preview: ''
+            preview: '',
+            exists: false
         }
     },
     components: {
-        Fabric,
         MainITY
     },
     methods: {
@@ -56,9 +55,16 @@ export default {
         },
         updateQuery: function() {
             this.preview = window.location.href + "ity?q=" + this.getQuery();
+            this.copy(this.preview);
+            this.exists = true;
         },
         objToString: function() {
             return JSON.stringify(this.message);
+        },
+        copy: function(preview_url) {
+            console.log("Custom I told you URL.");
+            console.log(preview_url);
+            alert("URL copied to clipboard");
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './registerServiceWorker'
+// import './registerServiceWorker'
+import OfficeUIFabricVue from 'office-ui-fabric-vue';
+import Clipboard from 'v-clipboard';
 
+Vue.use(OfficeUIFabricVue);
+Vue.use(Clipboard);
+
+// Styles
+import 'office-ui-fabric-vue/dist/index.css';
 import './assets/styles/fabric.min.css'
-// import Fabric from '@johannes-z/office-ui-fabric-vue'
-// import '@johannes-z/office-ui-fabric-vue/dist/office-ui-fabric-vue.css'
- 
-// Vue.use(Fabric)
-
-Vue.config.productionTip = false
 
 new Vue({
   router,
